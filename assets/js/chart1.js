d490640991bdde97214bc3b8cee33c0528352cae
Indonesia Melawan COVID-19 <algiffarirs@gmail.com>
2020-04-17 19:17:12 +0700

    Add files via upload

---
+++ assets/js/chart1.js
@@ -0,0 +1,25 @@
+var ctx = document.getElementById('chart1').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+        datasets: [{
+            label: 'Indonesia',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        },
+        {
+            label: 'Malaysia',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {maintainAspectRatio: false}
+});
